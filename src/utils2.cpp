41d3e4a24f794f03a425bfa467177c7c311802a5
masarunakajima <masarun@usc.edu>
2023-03-21 07:44:22 -0700

    defined is_valid_seq and base_to_num

--- src/utils2.cpp
+++ src/utils2.cpp
@@ -1,6 +1,8 @@
 #include "utils2.hpp"
 
 using std::map;
+using std::string;
+using std::vector;
 
 map<base, bnum> base_bnum_dict = {
   {'A', base_A}, {'a', base_A},
@@ -10,3 +12,28 @@ map<base, bnum> base_bnum_dict = {
   {'U', base_U}, {'u', base_T}
 };
 
+
+bool
+is_valid_seq (const string &bases) {
+
+  for (size_t i = 0; i < bases.size(); i++) {
+    auto it = base_bnum_dict.find(bases[i]);
+    if (it == base_bnum_dict.end()) {
+      return false;
+    } 
+  }
+  return true;
+}
+
+void 
+base_to_num (const string &bases, vector<bnum> num_seq) {
+
+  for (size_t i = 0; i < bases.size(); i++) {
+    auto it = base_bnum_dict.find(bases[i]);
+    if (it != base_bnum_dict.end()) {
+      num_seq.push_back(it->second);
+    } else {
+      num_seq.push_back(base_K);
+    }
+  }
+}
--- src/utils2.hpp
+++ src/utils2.hpp
@@ -18,10 +18,15 @@ const bnum base_G = 2;
 const bnum base_U = 3;
 const bnum base_T = 3;
 const bnum base_D = 6;
-const bnum base_K = 9;
+const bnum base_K = 9; // unknown base
 
 extern std::map<base, bnum> base_bnum_dict;
 
-void base_to_num (const std::string &bases, std::vector<bnum> num_seq);
+bool
+is_valid_seq (const std::string &bases);
+
+void 
+base_to_num (const std::string &bases, std::vector<bnum> num_seq);
+
 
 #endif
